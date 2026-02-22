d8685cee619f075d55a27b676973410092c9b80e
Sushanth <sushanthhebri336@gmail.com>
2026-02-23 02:41:12 +0530

    improve package and UI

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,4 +1,5 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+// Existing Imports
 import LandingPage from "./pages/LandingPage";
 import DashBoard from "./pages/Dashboard";
 import SignUpForOwner from "./pages/SignUpForOwner";
@@ -8,22 +9,58 @@ import ChooseTheRole from "./pages/ChooseTheRole";
 import SignupForDriver from "./pages/SignUpForDriver";
 import Pricing from "./pages/Pricing";
 
-const AppRoutes = () =>{
-    return(
-<Router>
-<Routes>
-<Route path="/" element={<LandingPage />} />
-<Route path="/signupforowner" element={<SignUpForOwner />} />
-<Route path="/login" element={<LoginPage />} />
-<Route path="/dashboard" element={<DashBoard />} />
-<Route path="/learnmore" element={<LearnMore />} />
-<Route path="/choosetherole" element={<ChooseTheRole />} />
-<Route path="/signupfordriver" element={<SignupForDriver />} />
-<Route path="/pricing" element={<Pricing />} />
-</Routes>
-</Router>
+// New Page Imports (To be created)
+import VehicleInventory from "./pages/vehicles/VehicleInventory";
+import VehicleDetails from "./pages/vehicles/VehicleDetails";
+import DriverManagement from "./pages/drivers/DriverManagement";
+import LiveTracking from "./pages/tracking/LiveTracking";
+import AnalyticsReports from "./pages/reports/AnalyticsReports";
+import Settings from "./pages/Settings";
+import NotFound from "./pages/NotFound";
 
-        );
-    };
+const AppRoutes = () => {
+  // Mock authentication check
+  const isAuthenticated = true; 
+
+  return (
+    <Router>
+      <Routes>
+        {/* PUBLIC ROUTES */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/learnmore" element={<LearnMore />} />
+        <Route path="/pricing" element={<Pricing />} />
+        
+        {/* AUTH ROUTES */}
+        <Route path="/choosetherole" element={<ChooseTheRole />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signupforowner" element={<SignUpForOwner />} />
+        <Route path="/signupfordriver" element={<SignupForDriver />} />
+
+        {/* PROTECTED DASHBOARD ROUTES */}
+        <Route 
+          path="/dashboard" 
+          element={isAuthenticated ? <DashBoard /> : <Navigate to="/login" />} 
+        />
+        
+        {/* VEHICLE MANAGEMENT */}
+        <Route path="/vehicles" element={<VehicleInventory />} />
+        <Route path="/vehicles/:id" element={<VehicleDetails />} /> {/* Dynamic Route */}
+        
+        {/* DRIVER MANAGEMENT */}
+        <Route path="/drivers" element={<DriverManagement />} />
+        
+        {/* MONITORING & ANALYTICS */}
+        <Route path="/live-tracking" element={<LiveTracking />} />
+        <Route path="/reports" element={<AnalyticsReports />} />
+        
+        {/* ACCOUNT & SYSTEM */}
+        <Route path="/settings" element={<Settings />} />
+
+        {/* 404 PAGE */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default AppRoutes;
